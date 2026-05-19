09d31234cb70820971db7811248bc29378aa0071
Karl Murphy <karlosskm89@gmail.com>
2026-05-19 12:45:41 +0100

    dev clean

--- asr-mail-relay/utils/verifyRequest.js
+++
@@ -1,5 +0,0 @@
-module.exports = function verifyRequest(req, expectedToken) {
-  const auth = req.headers.authorization || '';
-  const token = auth.replace(/^Bearer\s+/i, '');
-  return token && token === expectedToken;
-};
